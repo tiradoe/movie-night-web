dfc0f719e6bc8f9da128d814d54a3fa32ae18e50
Edward Tirado Jr <ed@etdevelopment.net>
2025-04-06 15:42:36 -0500

    added typescript

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,28 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-        head: {
-            charset: 'utf-8',
-            viewport: "width=device-width,initial-scale=1.0",
-            title: "Cinema Corona",
-            link: [
-                {rel: "icon", type: "image/png", href: "/favicon.png"}
-            ],
-        },
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width,initial-scale=1.0",
+      title: "Cinema Corona",
+      link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
     },
-    modules: ["@nuxtjs/tailwindcss"],
-    css: ["@/assets/css/main.css"],
-    components: {
-        dirs: [
-            '~/components',
-            '~/components/modal-content',
-            '~/components/forms',
-            '~/components/admin',
-        ]
+  },
+
+  modules: ["@nuxtjs/tailwindcss"],
+  css: ["@/assets/css/main.css"],
+  compatibilityDate: "2025-04-05",
+
+  components: {
+    dirs: [
+      "~/components",
+      "~/components/modal-content",
+      "~/components/forms",
+      "~/components/admin",
+    ],
+  },
+
+  runtimeConfig: {
+    public: {
+      apiURL: process.env.API_URL || "http://localhost:8000/api",
     },
-    runtimeConfig: {
-        public: {
-            apiURL: process.env.API_URL || "http://localhost:8000/api"
-        }
-    }
-})
+  },
+
+  typescript: {
+    typeCheck: true,
+  },
+});
