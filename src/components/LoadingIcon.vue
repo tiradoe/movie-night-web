b6036f8c702044a72525d49f702c8731b490e062
Edward Tirado Jr <ed@etdevelopment.net>
2025-04-22 21:22:11 -0500

    fixed safari loading icon...again

--- src/components/LoadingIcon.vue
+++ src/components/LoadingIcon.vue
@@ -2,13 +2,13 @@
 
 <template>
   <video
+    alt="Loading"
     autoplay
     class="flex absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
     loop
     muted
-    title="loading"
   >
-    <source src="/assets/img/movie-loader.mp4" type="video/mp4" />
+    <source src="/assets/img/movie-loader.mov" type="video/quicktime" />
     <source src="/assets/img/movie-loader.webm" type="video/webm" />
   </video>
 </template>
