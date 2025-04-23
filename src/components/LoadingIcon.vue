2e5568228521e738a8246b058a8d578a3b4beb2e
Edward Tirado Jr <ed@etdevelopment.net>
2025-04-23 12:52:06 -0500

    fixed safari loading icon...again

--- src/components/LoadingIcon.vue
+++ src/components/LoadingIcon.vue
@@ -7,6 +7,7 @@
     class="flex absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
     loop
     muted
+    playsinline
   >
     <source src="/assets/img/movie-loader.mov" type="video/quicktime" />
     <source src="/assets/img/movie-loader.webm" type="video/webm" />
