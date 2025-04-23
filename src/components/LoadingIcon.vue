ddea97e9cfc345d72f97a5113c4398a0862bd940
Edward Tirado Jr <ed@etdevelopment.net>
2025-04-22 20:38:48 -0500

    fixed safari loading icon

--- src/components/LoadingIcon.vue
+++ src/components/LoadingIcon.vue
@@ -9,10 +9,7 @@
     muted
     type="video/webm"
   >
-    <source
-      src="/assets/img/movie-loader.mp4"
-      type="video/mp4; codecs='hvc1'"
-    />
+    <source src="/assets/img/movie-loader.mov" type="video/mov" />
     <source src="/assets/img/movie-loader.webm" type="video/webm" />
   </video>
 </template>
