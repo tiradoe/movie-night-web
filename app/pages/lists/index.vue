66eddb56bf2e04001d1d6205488dcb7a428baccf
Edward Tirado Jr. <ed@etdevelopment.net>
2026-02-24 00:31:42 -0600

    removed unused v-for

--- app/pages/lists/index.vue
+++ app/pages/lists/index.vue
@@ -32,7 +32,7 @@ const refreshLists = () => {
     <!-- <div class="w-full flex flex-col gap-5">
       <h2 class="text-2xl font-bold">Shared With You</h2>
       <ul class="w-full flex flex-col gap-3">
-        <li v-for="list in lists.share" class="flex justify-between items-center p-4 bg-gray-700/50 rounded-lg hover:bg-gray-600/50 transition-colors">
+        <li class="flex justify-between items-center p-4 bg-gray-700/50 rounded-lg hover:bg-gray-600/50 transition-colors">
           <NuxtLink to="lists/2">Bob's List</NuxtLink>
         </li>
       </ul>
