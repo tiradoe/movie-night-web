26291062c0c33e89b13758eed3c4c2b9d1d0c58e
Edward Tirado Jr. <ed@etdevelopment.net>
2026-02-24 00:30:26 -0600

    lists page now pulls real data

--- app/pages/lists/index.vue
+++ app/pages/lists/index.vue
@@ -29,14 +29,15 @@ const refreshLists = () => {
       </ul>
     </div>
 
-    <div class="w-full flex flex-col gap-5">
+    <!-- <div class="w-full flex flex-col gap-5">
       <h2 class="text-2xl font-bold">Shared With You</h2>
       <ul class="w-full flex flex-col gap-3">
-        <li class="flex justify-between items-center p-4 bg-gray-700/50 rounded-lg hover:bg-gray-600/50 transition-colors">
+        <li v-for="list in lists.share" class="flex justify-between items-center p-4 bg-gray-700/50 rounded-lg hover:bg-gray-600/50 transition-colors">
           <NuxtLink to="lists/2">Bob's List</NuxtLink>
         </li>
       </ul>
     </div>
+    -->
   </div>
 
 </template>
