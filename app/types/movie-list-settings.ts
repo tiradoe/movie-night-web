47fc17636f609a9cca007dc7760a56fd2b349018
Edward Tirado Jr. <ed@etdevelopment.net>
2026-04-05 00:46:40 -0500

    removed unused type

--- app/types/movie-list-settings.ts
+++
@@ -1,9 +0,0 @@
-import type {User} from "~/types/user";
-import type {Role} from "~/types/role";
-
-export type MovieListSettings = {
-    listName: string,
-    isPublic: boolean,
-    collaborators: User[],
-    roles: Role[]
-}
